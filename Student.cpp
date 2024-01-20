12331ed682f809cf84facbe4585e58fa271fd9be
Zenuls <ivanov694420@gmail.com>
2024-01-20 12:45:28 +1000

    2

--- Student.cpp
+++ Student.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
+
 using namespace std;
 
 class Student{
     private:
-        string name;
-        string surname;
+        string firstName;
+        string lastName;
         vector<int> grades;
     public:
-        Student(string Newname, string Newsurname)
+        Student(string NewfirstName, string NewlastName)
         {
-            name = Newname;
-            surname = Newsurname;
+            firstName = NewfirstName;
+            lastName = NewlastName;
         }
-        string getname() { return name; }
+        string getfirstName() { return firstName; }
         vector<int> getgrades() { return grades; }
-        void add_grade(int NewGrade) { grades.push_back(NewGrade); }
-        bool Excelent_student()
+        void assignGrade(int NewGrade) { grades.push_back(NewGrade); }
+        bool isExcellentStudent()
         {
             float average;
             for(int i = 0; i < grades.size(); i++) average += grades[i];
@@ -31,12 +33,37 @@ class Teacher
 {
     private:
         string subject;
+        bool mood;
 
     public:
-        Teacher(string NewSubject) { this->subject = NewSubject; }
-        void giveGrade(Student& student, int grade){
-            student.add_grade(grade);
+        Teacher(string NewSubject) : subject(NewSubject)
+    {
+        mood = (rand() % 2 == 0);
+    }
+
+    void giveGrade(Student& student)
+    {
+        int randomGrade = 0;
+
+        if (mood && student.isExcellentStudent())
+        {
+            randomGrade = 5;
         }
+        else if (!mood && student.isExcellentStudent())
+        {
+            randomGrade = (rand() % 2 == 0) ? 4 : 5;
+        }
+        else if (mood && !student.isExcellentStudent())
+        {
+            randomGrade = 4;
+        }
+        else if (!mood && !student.isExcellentStudent())
+        {
+            randomGrade = (rand() % 2 == 0) ? 2 : 3;
+        }
+
+        student.assignGrade(randomGrade);
+    }
 
         string getSubject(){
             return this->subject;
@@ -45,13 +72,47 @@ class Teacher
 
 int main()
 {
-    Student s1("Misha", "Petrov");
-    Teacher t("Math");
-    t.giveGrade(s1, 5);
-    s1.add_grade(3);
-    s1.add_grade(2);
-    s1.add_grade(5);
-    cout << (s1.Excelent_student()?"Excellent":"Not excellent") << endl;
-    cout << t.getSubject() << endl;
+    srand(time(0));
+
+    Teacher t1("OOP");
+    Teacher t2("Math");
+    Teacher t3("English");
+
+    Student s1("Alex", "Mercer");
+    Student s2("James", "Franko");
+    Student s3("Peter", "Parker");
+
+    s1.assignGrade(3);
+    s1.assignGrade(2);
+    s1.assignGrade(5);
+
+    s2.assignGrade(5);
+    s2.assignGrade(4);
+    s2.assignGrade(5);
+
+    s3.assignGrade(4);
+    s3.assignGrade(4);
+    s3.assignGrade(4);
+
+    t1.giveGrade(s1);
+    t1.giveGrade(s2);
+    t1.giveGrade(s3);
+
+    t1.giveGrade(s1);
+    t1.giveGrade(s2);
+    t1.giveGrade(s3);
+
+    t3.giveGrade(s1);
+    t3.giveGrade(s2);
+    t3.giveGrade(s3);
+
+    cout << (s1.isExcellentStudent()?"Excellent":"Not excellent") << endl;
+    cout << (s2.isExcellentStudent()?"Excellent":"Not excellent") << endl;
+    cout << (s3.isExcellentStudent()?"Excellent":"Not excellent") << endl;
+
+    cout << t1.getSubject() << endl;
+    cout << t2.getSubject() << endl;
+    cout << t3.getSubject() << endl;
+
     return 0;
 }
